98541f4affedcc18d503bd90c724212cef5021a8
Thoang <nhothoang1994@gmail.com>
2023-08-26 17:27:35 +0700

    fix threading done

--- learn_english.py
+++ learn_english.py
@@ -2,14 +2,16 @@ import pygame
 import os
 import pyttsx4
 import threading
-
+lock = threading.Lock()
 def EN_voice(text):
-    engine = pyttsx4.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id) # change index to change voice
-    engine.setProperty('rate', 120) # change value to change speed
-    engine.say(text)
-    engine.runAndWait()
+    with lock:
+        engine = pyttsx4.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id) # change index to change voice
+        engine.setProperty('rate', 120)
+        engine.say(text)
+        engine.runAndWait()
+    
 def white_screen():
     whitescreen  = pygame.Rect(200,0 ,600, 100)
     pygame.draw.rect(screen, (255, 255, 255), whitescreen)
@@ -21,13 +23,14 @@ def background(file):
     return(screen)
 
 def show_text(text):
-    screen1 = white_screen()
-    font = pygame.font.SysFont('Comic Sans MS', 50)
-    text_surface = font.render(text, True, (255,0,0))
-    text_width, text_height = text_surface.get_size()
-    white_screen_x = screen1.x + screen1.width / 2
-    white_screen_y = screen1.y + screen1.height / 2
-    screen.blit(text_surface, (white_screen_x - text_width / 2, white_screen_y - text_height / 2))
+    with lock:
+        screen1 = white_screen()
+        font = pygame.font.SysFont('Comic Sans MS', 50)
+        text_surface = font.render(text, True, (255,0,0))
+        text_width, text_height = text_surface.get_size()
+        white_screen_x = screen1.x + screen1.width / 2
+        white_screen_y = screen1.y + screen1.height / 2
+        screen.blit(text_surface, (white_screen_x - text_width / 2, white_screen_y - text_height / 2))
 
 def load_images(path_to_directory):
     image_dict = {}
@@ -51,12 +54,6 @@ def button(background1, imagebutton):
         images[str(current_image + 1)] = images[str(current_image + 1)].convert_alpha()
         screen.blit(images[str(current_image + 1)], (i* 100, 250))
 
-
-    # for filename in range(10):
-    #     current_image = int(filename)
-    #     images[str(current_image + 1)] = images[str(current_image + 1)].convert_alpha()
-    #     screen.blit(images[str(current_image + 1)], (current_image * 100, 250))
-   
 def txt_list():
     with open('./text/animal.txt','r') as file:
         i = "".join(str(j)for j in file).replace('\n', ',')
@@ -71,14 +68,14 @@ def loop_event(file):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for i in range(10):
                     if event.button == 1 and i <= event.pos[0] // 100 < i+1 and \
-                    150 <= event.pos[1] <= pygame.image.load(f'{file}{i+1}.png').get_height()+150:                
-                        threading.Thread(target=show_text(txt_list()[i])).start()
-                        threading.Thread(target=EN_voice(txt_list()[i])).start()
+                    150 <= event.pos[1] <= pygame.image.load(f'{file}{i+1}.png').get_height()+150: 
+                        threading.Thread(target=show_text, args=(txt_list()[i],)).start()
+                        threading.Thread(target=EN_voice, args=(txt_list()[i],)).start()
                 for i,j in enumerate(range(10,20)):
                     if event.button == 1 and i <= event.pos[0] // 100 < i+1 and \
-                    250 <= event.pos[1] <= pygame.image.load(f'{file}{j+1}.png').get_height()+250:               
-                        threading.Thread(target=show_text(txt_list()[j])).start()
-                        threading.Thread(target=EN_voice(txt_list()[j])).start()                                         
+                    250 <= event.pos[1] <= pygame.image.load(f'{file}{j+1}.png').get_height()+250:
+                        threading.Thread(target=show_text, args=(txt_list()[j],)).start()
+                        threading.Thread(target=EN_voice, args=(txt_list()[j],)).start()                                      
         pygame.display.update()
 if __name__=="__main__":
     pygame.init()
